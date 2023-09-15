13738228f69098dbb27ef916b1d4061306357588
yhlee-efx <yhlee@efinixinc.com>
2023-09-15 13:57:44 +0800

    Added support for Ti180J484 devkit.

---
+++ Ti180J484_hdmi/source/hw_accel/hw_accel.v
@@ -0,0 +1,102 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 github-efx
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////////
+
+module hw_accel #(
+   parameter DATA_WIDTH    = 8,
+   parameter IMG_WIDTH     = 512,
+   parameter IMG_HEIGHT    = 512
+)(
+   input  wire                  clk,
+   input  wire                  rst,
+   input  wire [1:0]            hw_accel_mode,
+   input  wire [DATA_WIDTH-1:0] sobel_thresh_val,
+   input  wire [DATA_WIDTH-1:0] pixel_in,
+   input  wire                  pixel_in_valid,
+   output reg  [DATA_WIDTH-1:0] pixel_out,
+   output reg                   pixel_out_valid
+);
+
+   wire [DATA_WIDTH-1:0] sobel_pixel_out;
+   wire                  sobel_pixel_out_valid;
+   wire [DATA_WIDTH-1:0] erosion_pixel_out;
+   wire                  erosion_pixel_out_valid;
+   wire [DATA_WIDTH-1:0] dilation_pixel_out;
+   wire                  dilation_pixel_out_valid;
+
+   hw_accel_sobel_edge_detection 
+   # (
+      .DATA_WIDTH (DATA_WIDTH),
+      .IMG_WIDTH  (IMG_WIDTH),
+      .IMG_HEIGHT (IMG_HEIGHT)
+   ) u_sobel (
+      .clk               (clk),
+      .rst               (rst),
+      .sobel_thresh_val  (sobel_thresh_val),
+      .pixel_in          (pixel_in),
+      .pixel_in_valid    (pixel_in_valid),
+      .pixel_out         (sobel_pixel_out),
+      .pixel_out_valid   (sobel_pixel_out_valid)
+   );
+   
+   hw_accel_binary_dilation 
+   # (
+      .DATA_WIDTH (DATA_WIDTH),
+      .IMG_WIDTH  (IMG_WIDTH),
+      .IMG_HEIGHT (IMG_HEIGHT)
+   ) u_dilation (
+      .clk               (clk),
+      .rst               (rst),
+      .pixel_in          (sobel_pixel_out),
+      .pixel_in_valid    (sobel_pixel_out_valid),
+      .pixel_out         (dilation_pixel_out),
+      .pixel_out_valid   (dilation_pixel_out_valid)
+   );
+   
+   hw_accel_binary_erosion 
+   # (
+      .DATA_WIDTH (DATA_WIDTH),
+      .IMG_WIDTH  (IMG_WIDTH),
+      .IMG_HEIGHT (IMG_HEIGHT)
+   ) u_erosion (
+      .clk               (clk),
+      .rst               (rst),
+      .pixel_in          (sobel_pixel_out),
+      .pixel_in_valid    (sobel_pixel_out_valid),
+      .pixel_out         (erosion_pixel_out),
+      .pixel_out_valid   (erosion_pixel_out_valid)
+   );
+
+   //2'd0: Sobel only; 2'd1: Sobel+Dilation; Otherwise: Sobel+Erosion
+   always@(posedge clk or posedge rst)
+   begin
+      if (rst) begin
+         pixel_out       <= {DATA_WIDTH{1'b0}};
+         pixel_out_valid <= 1'b0;
+      end else begin
+         pixel_out       <= (hw_accel_mode == 2'd0) ? sobel_pixel_out          : 
+                            (hw_accel_mode == 2'd1) ? dilation_pixel_out       : erosion_pixel_out;
+         pixel_out_valid <= (hw_accel_mode == 2'd0) ? sobel_pixel_out_valid    : 
+                            (hw_accel_mode == 2'd1) ? dilation_pixel_out_valid : erosion_pixel_out_valid;
+      end
+   end
+
+endmodule
---
+++ Ti180J484_hdmi/source/hw_accel/hw_accel_line_buffer.v
@@ -0,0 +1,95 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 github-efx
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////////
+
+module hw_accel_line_buffer #(
+   parameter DATA_WIDTH  = 8,
+   parameter LINE_WORDS  = 10
+)(
+   input  wire                  clk,
+   input  wire                  rst,
+   input  wire                  en,
+   input  wire [DATA_WIDTH-1:0] din,
+   output wire [DATA_WIDTH-1:0] dout_tap0,
+   output wire [DATA_WIDTH-1:0] dout_tap1,
+   output reg  [DATA_WIDTH-1:0] dout_tap2,
+   //output wire                  dout_valid
+   output wire                  dout_padded_valid
+);
+
+   localparam ADDR_WIDTH = $clog2 (LINE_WORDS);
+   
+   reg  [ADDR_WIDTH-1:0] addr;
+   reg                   alternate;
+   reg [ADDR_WIDTH+1:0]  latency_count;
+   //reg                   latency_done;
+   reg                   latency_padded_done;
+   reg                   en_r;
+   
+   //assign dout_valid        = en_r && latency_done;
+   assign dout_padded_valid = en_r && latency_padded_done;
+   
+   hw_accel_true_dual_port_ram #(
+      .DATA_WIDTH    (DATA_WIDTH),
+      .ADDR_WIDTH    (ADDR_WIDTH+1),
+      .WRITE_MODE_1  ("READ_FIRST"),
+      .WRITE_MODE_2  ("READ_FIRST"),
+      .OUTPUT_REG_1  ("FALSE"),
+      .OUTPUT_REG_2  ("FALSE"),
+      .RAM_INIT_FILE ("")
+   ) u_ram (
+      .we1   (en),
+      .clka  (clk),
+      .din1  (din),
+      .addr1 ({alternate, addr}),
+      .dout1 (dout_tap0),
+
+      .we2   (1'b0),
+      .clkb  (clk),
+      .din2  ({DATA_WIDTH{1'b0}}),
+      .addr2 ({~alternate, addr}),
+      .dout2 (dout_tap1)
+   );
+   
+   always@(posedge clk or posedge rst)
+   begin
+      if (rst) begin
+         dout_tap2           <= {DATA_WIDTH{1'b0}};
+         addr                <= {ADDR_WIDTH{1'b0}};
+         alternate           <= 1'b0;
+         latency_count       <= {ADDR_WIDTH+2{1'b0}};
+         //latency_done        <= 1'b0;
+         latency_padded_done <= 1'b0;
+         en_r                <= 1'b0;
+      end else begin
+         dout_tap2           <= din;
+         addr                <= (en && (addr==LINE_WORDS-1)) ? {ADDR_WIDTH{1'b0}} :
+                                (en)                         ? addr + {{ADDR_WIDTH-1{1'b0}}, 1'b1}
+                                                             : addr;
+         alternate           <= (en && (addr==LINE_WORDS-1)) ? ~alternate : alternate;
+         latency_count       <= (en) ? latency_count + {{ADDR_WIDTH+1{1'b0}}, 1'b1} : latency_count;
+         //latency_done        <= ((latency_count == (LINE_WORDS*2)) && en) ? 1'b1 : latency_done;        //LINE_COUNT*M where M =(kernel_height-1)/2 + 1;
+         latency_padded_done <= ((latency_count == (LINE_WORDS*1)) && en) ? 1'b1 : latency_padded_done;   //LINE_COUNT*M where M =(kernel_height-1)/2;
+         en_r                <= en;
+      end
+   end
+  
+endmodule
